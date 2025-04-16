6b997dbf5e554752015d646b673eb35bec7e2f48
murat <muratmutlu010@gmail.com>
2025-04-16 18:56:46 +0300

    feat: configure TypeORM for database connection in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { configModuleOptions } from '../config/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmModuleAsyncOptions } from 'src/database/data-source';
 
 @Module({
-  imports: [ConfigModule.forRoot(configModuleOptions)],
+  imports: [
+    ConfigModule.forRoot(configModuleOptions),
+    TypeOrmModule.forRootAsync(typeOrmModuleAsyncOptions),
+  ],
   controllers: [],
   providers: [],
 })
